aaa64c1ccb2883727c8f5379ca9cf3a618ede918
Camilo Rincon <camilo.rincon@globant.com>
2017-10-06 16:22:11 -0500

    Testing changes

--- test/screens/HomeScreen.js
+++ test/screens/HomeScreen.js
@@ -8,6 +8,7 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
+import { styles } from './styles';
 import { WebBrowser } from 'expo';
 
 import { MonoText } from '../components/StyledText';
@@ -37,20 +38,24 @@ export default class HomeScreen extends React.Component {
           <View style={styles.getStartedContainer}>
             {this._maybeRenderDevelopmentModeWarning()}
 
-            <Text style={styles.getStartedText}>Get started by opening</Text>
+            <Text style={styles.getStartedText}>PublicFlex Busca lo que necesitas </Text>
 
             <View
               style={[
                 styles.codeHighlightContainer,
                 styles.homeScreenFilename,
               ]}>
-              <MonoText style={styles.codeHighlightText}>
+              {/* <MonoText style={styles.codeHighlightText}>
                 screens/HomeScreen.js
-              </MonoText>
+              </MonoText> */}
             </View>
 
             <Text style={styles.getStartedText}>
-peerra            </Text>
+            Cami, Kt, Maggie
+            </Text>
+            <Text style={styles.getStartedText}>
+            We Rock!!
+            </Text>
           </View>
 
           <View style={styles.helpContainer}>
@@ -66,7 +71,7 @@ peerra            </Text>
 
         <View style={styles.tabBarInfoContainer}>
           <Text style={styles.tabBarInfoText}>
-            This is a tab bar. You can edit it in:
+            This is a tab bar. You can edit it in: We can add here many things
           </Text>
 
           <View
@@ -97,7 +102,7 @@ peerra            </Text>
     } else {
       return (
         <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
+          You are not in development mode, your app will run at full speed, testing app iOS - Android.
         </Text>
       );
     }
@@ -115,92 +120,3 @@ peerra            </Text>
     );
   };
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-});
